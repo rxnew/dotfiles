6f7c03b9269811e9291b724b7a345dd901e7ee48
Kotaro Hoshi <rxnew@k.hoshi>
2019-04-18 09:24:20 +0900

    add gitignore_global

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 readonly PATH_TMUX_CONF=$HOME/.tmux.conf
+readonly PATH_GITIGNORE_GLOBAL=$HOME/.gitignore_global
 
 cd $(dirname $0)
 
 # tmux
 [ -e $PATH_TMUX_CONF ] && mv $PATH_TMUX_CONF ${PATH_TMUX_CONF}.bak
 cp tmux.conf $PATH_TMUX_CONF
+echo 'Install .tmux.conf'
+
+# Git
+[ -e $PATH_GITIGNORE_GLOBAL ] && mv $PATH_GITIGNORE_GLOBAL ${PATH_GITIGNORE_GLOBAL}.bak
+cp gitignore_global $PATH_GITIGNORE_GLOBAL
+git config --global core.excludesfile ~/.gitignore_global \
+    || echo 'Please enable to .gitignore_global: git config --global core.excludesfile ~/.gitignore_global'
+echo 'Install .gitignore_global'
