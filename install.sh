ce8736e2b1f2484b60aed7c47905a910655c15ff
rxnew <rxnew.axdseuan+a@gmail.com>
2019-08-23 12:45:18 +0900

    update tmux.conf

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-readonly PATH_TMUX_CONF=$HOME/.tmux.conf
 readonly PATH_GITIGNORE_GLOBAL=$HOME/.gitignore_global
+readonly PATH_TMUX_CONF=$HOME/.tmux.conf
+readonly PATH_TMUX_PLUGINS=$HOME/.tmux/plugins
 
 cd $(dirname $0)
-
-# tmux
-[ -e $PATH_TMUX_CONF ] && mv $PATH_TMUX_CONF ${PATH_TMUX_CONF}.bak
-cp tmux.conf $PATH_TMUX_CONF
-echo 'Install .tmux.conf'
 
 # Git
 [ -e $PATH_GITIGNORE_GLOBAL ] && mv $PATH_GITIGNORE_GLOBAL ${PATH_GITIGNORE_GLOBAL}.bak
@@ -16,3 +12,10 @@ cp gitignore_global $PATH_GITIGNORE_GLOBAL
 git config --global core.excludesfile ~/.gitignore_global \
     || echo 'Please enable to .gitignore_global: git config --global core.excludesfile ~/.gitignore_global'
 echo 'Install .gitignore_global'
+
+# tmux
+[ -e $PATH_TMUX_CONF ] && mv $PATH_TMUX_CONF ${PATH_TMUX_CONF}.bak
+cp tmux.conf $PATH_TMUX_CONF
+[ -e $PATH_TMUX_PLUGINS/tpm ] && mv $PATH_TMUX_PLUGINS/tpm $PATH_TMUX_PLUGINS/tpm.bak
+git clone https://github.com/tmux-plugins/tpm $PATH_TMUX_PLUGINS/tpm
+echo 'Install .tmux.conf'
